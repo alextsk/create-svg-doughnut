3ae38c84b67519ab6ab074242ad8e681bcbaaef1
Alex <16453186+alextsk@users.noreply.github.com>
2018-08-28 18:35:16 +0700

    new build

--- dist/createSVGDoughnut.js
+++ dist/createSVGDoughnut.js
@@ -1,48 +1,37 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+import createSVGPie from 'create-svg-pie';
 
-var _createSvgPie = require('create-svg-pie');
+function createSVGDoughnut(data, outerRadius, innerRadius = outerRadius * 0.8, palette) {
+    const pie = createSVGPie(data, outerRadius, palette);
 
-var _createSvgPie2 = _interopRequireDefault(_createSvgPie);
+    const defs = document.createElementNS("http://www.w3.org/2000/svg", "defs");
+    const mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
+    mask.id = "hole" + Math.floor(Math.random() * 10000); // fix, was simply hole
 
-function createSVGDoughnut(data, outerRadius, innerRadius, palette) {
-    if (innerRadius === undefined) innerRadius = outerRadius * 0.8;
-    return (function () {
-        var pie = (0, _createSvgPie2["default"])(data, outerRadius, palette);
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute("cx", outerRadius);
+    circle.setAttribute("cy", outerRadius);
+    circle.setAttribute("r", innerRadius);
 
-        var defs = document.createElementNS("http://www.w3.org/2000/svg", "defs");
-        var mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
-        mask.id = "hole";
+    const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect.setAttribute("width", '100%');
+    rect.setAttribute("height", '100%');
+    rect.setAttribute("fill", "white");
 
-        var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        circle.setAttribute("cx", outerRadius);
-        circle.setAttribute("cy", outerRadius);
-        circle.setAttribute("r", innerRadius);
+    defs.appendChild(mask);
+    mask.appendChild(rect);
+    mask.appendChild(circle);
 
-        var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        rect.setAttribute("width", '100%');
-        rect.setAttribute("height", '100%');
-        rect.setAttribute("fill", "white");
-
-        defs.appendChild(mask);
-        mask.appendChild(rect);
-        mask.appendChild(circle);
-
-        var childElementCount = pie.childElementCount;
-        for (var i = 0; i < childElementCount; i++) {
-            if (pie.childNodes[i].setAttribute) {
-                pie.childNodes[i].setAttribute('mask', 'url(#hole)');
-            }
+    const childElementCount = pie.childElementCount;
+    for (var i = 0; i < childElementCount; i++) {
+        if (pie.childNodes[i].setAttribute) {
+            pie.childNodes[i].setAttribute('mask', `url(#${ mask.id })`); // fix, was simply #hole
         }
+    }
 
-        pie.appendChild(defs);
-        return pie;
-    })();
+    pie.appendChild(defs);
+    return pie;
 }
 
-exports["default"] = createSVGDoughnut;
-module.exports = exports["default"];
+export default createSVGDoughnut;
